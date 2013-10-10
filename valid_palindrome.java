831f686d62cdfd76ba1644c71127265db14b9989
yalin <ywang28@ncsu.edu>
2013-10-09 20:03:46 -0700

    valid palindrome sol1

---
+++ valid_palindrome.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0)  return true;
+        int start = -1;
+        int end = s.length();
+        while(++start <= --end)  {
+            while(start<s.length() && !Character.isLetterOrDigit(s.charAt(start)))  start++;
+            while(end>=0 && !Character.isLetterOrDigit(s.charAt(end)))  end--;
+            if(start>end)  return true;
+            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))  return false;
+        }
+        return true;
+    }
+}

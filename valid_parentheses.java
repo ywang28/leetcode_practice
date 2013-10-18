e46f308c5eaa495a1b3d1f17b5397f119ea05616
yalin <ywang28@ncsu.edu>
2013-10-17 21:11:04 -0700

    valid parentheses

---
+++ valid_parentheses.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean isValid(String s) {
+        if(s==null || s.length()==0)    return true;
+        // if odd number of parens
+        if(s.length()%2!=0)   return false;
+        LinkedList<Character> openParen = new LinkedList<Character>();
+        for(int i=0; i<s.length(); i++)  {
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{')  {
+                openParen.push(s.charAt(i));
+            }
+            else if(s.charAt(i)==')')  {
+                if (openParen.isEmpty() || openParen.pop()!='(')
+                    return false;
+            }
+            else if(s.charAt(i)==']')  {
+                if (openParen.isEmpty() || openParen.pop()!='[')
+                    return false;
+            }
+            else  {
+                if (openParen.isEmpty() || openParen.pop()!='{')
+                    return false;
+            }
+        }
+        return openParen.isEmpty();
+    }
+}

86e38d38bec624a3748b5b669d919e769b0e973e
yalin <ywang28@ncsu.edu>
2013-10-20 10:39:18 -0700

    search insert position

---
+++ search_insert_position.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        if(A==null || A.length==0)  return 0;
+        int start = 0;
+        int end = A.length-1;
+        int mid;
+        while(start<=end)  {
+            mid = start + (end-start)/2;
+            if(A[mid]==target)  return mid;
+            if(A[mid]>target)  {
+                end = mid-1;
+            }
+            else  {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}

2eac83888b4a2f86493cf2da5be8074c4e2412c8
yalin <ywang28@ncsu.edu>
2013-10-30 19:27:42 -0700

    dp solution

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -1,12 +1,22 @@
-// Time limit exceeded
-public class MinPathSum {
+// 2nd try. Pass
+public class Solution {
     public int minPathSum(int[][] grid) {
-        if(grid==null)  return 0;
-        return findPathSum(grid,0,0);
+        if(grid==null || grid.length==0 || grid[0].length==0)  return 0;
+        int xLen = grid.length;
+        int yLen = grid[0].length;
+        int[][] dp = new int[xLen][yLen];
+        for(int i=0; i<xLen; i++)  {
+            for(int j=0; j<yLen; j++)  {
+                dp[i][j] = -1;
+            }
+        }
+        dp[xLen-1][yLen-1] = grid[xLen-1][yLen-1];
+        return findPathSum(grid,dp,0,0);
     }
-    public int findPathSum(int[][] grid, int x, int y)  {
+    public int findPathSum(int[][] grid, int[][] dp, int x, int y)  {
         if(x > grid.length-1 || y > grid[0].length-1)  return Integer.MAX_VALUE;
-        if(x==grid.length-1 && y==grid[0].length-1)  return grid[x][y];
-        return grid[x][y] + Math.min(findPathSum(grid,x+1,y), findPathSum(grid,x,y+1));
+        if(dp[x][y]!=-1)  return dp[x][y];
+        dp[x][y] = grid[x][y] + Math.min(findPathSum(grid,dp,x+1,y), findPathSum(grid,dp,x,y+1));
+        return dp[x][y];
     }
 }

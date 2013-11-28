23296412a3755816bf5377017ba31c4ba723aaac
yalin <ywang28@ncsu.edu>
2013-11-28 08:03:54 -0800

    merge sorted array

---
+++ MergeSortedArray.java
@@ -0,0 +1,18 @@
+public class MergeSortedArray {
+    public void merge(int A[], int m, int B[], int n) {
+        if(A==null || A.length==0 || B==null || B.length==0)  return;
+        int i = m-1, j = n-1;
+        // fill in the result backwords
+        for(int k = m+n-1; k>=0; k--)  {
+            if(i < 0)  {
+                A[k] = B[j--];
+            }
+            else if(j < 0)  {
+                A[k] = A[i--];
+            }
+            else  {
+                A[k] = (A[i] > B[j]) ? A[i--] : B[j--];
+            }
+        }
+    }
+}

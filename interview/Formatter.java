e22a5789eb5c12cac9d3de347ad86c0e74a1bd20
yalin <ywang28@ncsu.edu>
2014-01-03 11:46:00 -0800

    flat function update

--- interview/Formatter.java
+++ interview/Formatter.java
@@ -4,8 +4,8 @@
 import java.util.ArrayList;
 
 public final class Formatter  {
-    public static void puts(String s)  {
-        System.out.println(s);
+    public static String flat(String s)  {
+        return s;
     }
     public static String flat(int[] in)  {
         String ret = "";
@@ -14,13 +14,22 @@ public final class Formatter  {
                 ret += n + " ";
             }
         }
-        return ret;
+        return ret.trim();
+    }
+    public static String flat(int[][] in)  {
+        String ret = "";
+        if(in != null)  {
+            for(int[] n : in)  {
+                ret += "\n" + flat(n);
+            }
+        }
+        return ret.replaceAll(" ","\t");
     }
     public static String flat(ArrayList<ArrayList<Integer>> in)  {
         String ret = "";
         if(in != null)  {
             for(ArrayList<Integer> t : in)  {
-                ret += "[" + flatList(t) + "]" + ",";
+                ret += "\n" + flatList(t);
             }
         }
         return ret;
@@ -32,6 +41,6 @@ public final class Formatter  {
                 ret += t + " ";
             }
         }
-        return ret;
+        return "[" + ret.trim() + "]";
     }
 }

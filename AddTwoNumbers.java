073959de6ab94409fd12554d979f67c6b30687e9
yalin <ywang28@ncsu.edu>
2014-01-06 22:18:48 -0800

    add two numbers 2nd

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -33,4 +33,36 @@ public class AddTwoNumbers {
         }
         return res.next;
     }
+    public ListNode addTwoNumbers2nd(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        
+        ListNode dummy = new ListNode(0), curr = dummy;
+        int carry = 0;
+        
+        while(l1!=null || l2!=null)  {
+            int val1=0, val2=0;
+            if(l1!=null)  {
+                val1 = l1.val;
+                l1 = l1.next;
+            }
+            if(l2!=null)  {
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+            int sum = val1 + val2 + carry;
+            curr.next = new ListNode(sum%10);
+            carry = sum/10;
+            curr = curr.next;
+        }
+        
+        // last carry
+        if(carry>0)  {
+            curr.next = new ListNode(carry);
+            curr = curr.next;
+        }
+        curr.next = null;
+        
+        return dummy.next;
+    }
 }

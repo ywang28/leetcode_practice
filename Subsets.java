297b84ebfb0ccaf01260037f1a1c50d7347a94ae
yalin <ywang28@ncsu.edu>
2014-02-03 22:37:58 -0800

    subsets 2nd solution

--- Subsets.java
+++ Subsets.java
@@ -25,4 +25,23 @@ public class Subsets {
         }
         return sublists;
     }
+    public ArrayList<ArrayList<Integer>> subsets2(int[] S) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        if(S==null)  return ret;
+        Arrays.sort(S);
+        int len = S.length, max = 1<<len;
+        // count from 0 to pow(2,len) - 1. Each bit represent if certain num is selected
+        for(int i=0; i<max; i++)  {
+            ArrayList<Integer> curr = new ArrayList<Integer>();
+            // get each bit of i and add corresponding num
+            for(int j=i, index=0; j>0; j>>=1, index++)  {
+                if((j & 1) == 1)  {
+                    curr.add(S[index]);
+                }
+            }
+            ret.add(curr);
+        }
+        return ret;
+    }
 }

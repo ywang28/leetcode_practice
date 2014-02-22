b9e9513e23b6abfdea0292e8435808b8425bccdf
yalin <ywang28@ncsu.edu>
2014-02-21 20:07:39 -0800

    valid palindrome 2nd

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -11,4 +11,25 @@ public class ValidPalindrome {
         }
         return true;
     }
+    public boolean isPalindrome(String s) {
+        if(s==null || s.length()==0)  return true;
+        int i=0, j=s.length()-1;
+        String s2 = s.toLowerCase();
+        while(i<=j)  {
+            if(!Character.isLetterOrDigit(s2.charAt(i)))  {
+                i++;
+            }
+            else if(!Character.isLetterOrDigit(s2.charAt(j)))  {
+                j--;
+            }
+            else if(s2.charAt(i)!=s2.charAt(j))  {
+                return false;
+            }
+            else  {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
 }

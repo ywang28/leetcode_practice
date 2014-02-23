169f306141ef37badbf306c6c9de5b066866af8a
yalin <ywang28@ncsu.edu>
2014-02-22 22:06:38 -0800

    single number

---
+++ SingleNumber.java
@@ -0,0 +1,10 @@
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+        if(A==null || A.length==0)  return -1;
+        int ret = A[0];
+        for(int i=1; i<A.length; i++)  {
+            ret ^= A[i];
+        }
+        return ret;
+    }
+}

69a163876a7bfbb8fa2ff8c6c31bffb280da03da
yalin <ywang28@ncsu.edu>
2014-03-14 00:43:47 -0700

    permutation sequence pass

--- PermutationSequence.java
+++ PermutationSequence.java
@@ -1,21 +1,37 @@
-// Wrong answer
+// pass
 public class PermutationSequence {
     public String getPermutation(int n, int k) {
-        int fac = fact(n);
-        if(k<1 || k>fac)  return "";
-        if(n==1)  return "1";
-        String ans = "";
-        int pre = 0;
-        int curr = ( k - 1 ) / ( fac / n ) + 1;
-        if(pre != 0 && curr >= pre)  {
-            curr++;
+        if(n<1 || n>9)  return "";
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++)  {
+            nums[i] = i+1;
         }
-        ans += curr;
-        ans += getPermutation(n-1, ( k - 1 ) % ( fac / n ) + 1);
-        return ans;
+        int total = fact(n), divider = total, index;
+        if(k<1 || k>total)  return "";
+        // remove off by 1
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for(int i=n; i>=1; i--)  {
+            // get number of permutations for i-1 nums
+            divider /= i;
+            index = k/divider;
+            k = k%divider;
+            sb.append(nums[index]);
+            nums = removeNum(nums, index);
+        }
+        return sb.toString();
     }
     public int fact(int n)  {
         if(n==1)  return 1;
-        return n * fact(n-1);
+        return fact(n-1)*n;
+    }
+    // remove selected number from array
+    public int[] removeNum(int[] nums, int index)  {
+        int[] ret = new int[nums.length-1];
+        for(int i=0, j=0; i<nums.length; i++)  {
+            if(i==index)  continue;
+            ret[j++] = nums[i];
+        }
+        return ret;
     }
 }

9a2c67ae147603d07ee4a96620455bb64e787177
yalin <ywang28@ncsu.edu>
2014-03-22 00:24:47 -0700

    subsets 2 pass

--- Subsets2.java
+++ Subsets2.java
@@ -1,23 +1,32 @@
-// wrong answer
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Subsets2 {
     public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
         ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
         if(num==null || num.length==0)  return ret;
         Arrays.sort(num);
-        subsetsHelper(num, 0, new ArrayList<Integer>(), ret);
-        ret.add(new ArrayList<Integer>());
+        subsetsHelper(num, 0, ret, new ArrayList<Integer>());
         return ret;
     }
-    public void subsetsHelper(int[] num, int index, ArrayList<Integer> curr, ArrayList<ArrayList<Integer>> ret)  {
-        // DFS search
+    public void subsetsHelper(int[] num, int index, ArrayList<ArrayList<Integer>> ret, ArrayList<Integer> path)  {
+        ret.add(new ArrayList<Integer>(path));
         for(int i=index; i<num.length; i++)  {
-            // repeated num found
+            // remove duplicates
             if(i>index && num[i]==num[i-1])  continue;
-            curr.add(num[i]);
-            ret.add(new ArrayList<Integer>(curr));
-            subsetsHelper(num, index+1, curr, ret);
-            curr.remove(curr.size()-1);
+            path.add(num[i]);
+            subsetsHelper(num, i+1, ret, path);
+            path.remove(path.size()-1);
         }
     }
+    public static void main(String[] args)  {
+        Subsets2 s = new Subsets2();
+        s.subsetsWithDup(new int[]{1,2,3,4,5,6,7,0});
+        for(ArrayList<Integer> aa : s.subsetsWithDup(new int[]{1,2,3,4,5,6,7,0}))  {
+            for(int n : aa)  {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }    
 }

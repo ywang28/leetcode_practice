d27a023663fa7917645635f8aac8a5c58634a3cf
yalin <ywang28@ncsu.edu>
2014-04-08 23:25:11 -0700

    sort list

--- SortList.java
+++ SortList.java
@@ -1,32 +1,34 @@
-// wrong answer
 public class SortList {
     public ListNode sortList(ListNode head) {
         if(head==null || head.next==null)  return head;
-        ListNode dummy = new ListNode(0), end=head;
-        dummy.next = head;
-        // find the last node
-        while(end.next!=null)  end = end.next;
-        quickSort(dummy, end);
-        return dummy.next;
-    }
-    public void quickSort(ListNode start, ListNode end)  {
-        if(start.next == end)  return;
-        ListNode pivot = start.next, pos = pivot, curr = pivot;
-        while(curr != end)  {
-            if(curr.next.val < pivot.val)  {
-                int tmp = curr.val;
-                curr.val = pos.next.val;
-                pos.next.val = tmp;
-                pos = pos.next;
+        // cut the list into halves and sort separately
+        ListNode mid = findMid(head);
+        ListNode left = sortList(head), right = sortList(mid);
+        // merge the generated list
+        ListNode curr = new ListNode(0), dummy = curr;
+        while(left!=null && right!=null)  {
+            if(left.val < right.val) {
+                curr.next = left;
+                left = left.next;
+            }
+            else  {
+                curr.next = right;
+                right = right.next;
             }
             curr = curr.next;
         }
-        // swap pivot
-        int tmp = pos.next.val;
-        pos.next.val = pivot.val;
-        pivot.val = tmp;
-        // recursively sort
-        quickSort(start, pos);
-        quickSort(pos.next, end);
+        curr.next = (left==null) ? right : left;
+        return dummy.next;
+    }
+    public ListNode findMid(ListNode head)  {
+        ListNode prev = new ListNode(0), slow = head, fast = head;
+        while(fast!=null && fast.next!=null)  {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // cut off the prev half list
+        prev.next = null;
+        return slow;
     }
 }

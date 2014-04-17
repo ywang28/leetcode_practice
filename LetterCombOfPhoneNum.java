c8ca3d39041223e010412c5334a0ea79763cbe25
yalin <ywang28@ncsu.edu>
2014-04-17 08:27:03 -0700

    letter comb of phone num 2nd

--- LetterCombOfPhoneNum.java
+++ LetterCombOfPhoneNum.java
@@ -24,4 +24,30 @@ public class LetterCombOfPhoneNum {
         }
         return comb;
     }
+    public ArrayList<String> letterCombinations2(String digits) {
+        if(digits==null)  {
+            ArrayList<String> ret = new ArrayList<String>();
+            ret.add("");
+            return ret;
+        }
+        String[] dict = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        return combHelper(digits, digits.length()-1, dict);
+    }
+    public ArrayList<String> combHelper(String digits, int index, String[] dict)  {
+        ArrayList<String> ret = new ArrayList<String>();
+        // base case
+        if(digits==null || index==-1)  {
+            ret.add("");
+            return ret;
+        }
+        int num = digits.charAt(index) - '0';
+        // error
+        if(num<2 || num>9)  return ret;
+        for(String s : combHelper(digits, index-1, dict))  {
+            for(char c : dict[num].toCharArray())  {
+                ret.add(s + c);
+            }
+        }
+        return ret;
+    }
 }

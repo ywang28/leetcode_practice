7b1ac7f9d9009fddbc9adcc6cee2d7a7d5a7929b
yalin <ywang28@ncsu.edu>
2014-04-19 15:07:24 -0700

    swap nodes in pairs 2nd

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -24,4 +24,21 @@ public class SwapNodesInPairs {
         }
         return ret;
     }
+    public ListNode swapPairs2(ListNode head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if(head==null || head.next==null)  return head;
+        ListNode dummy = new ListNode(0), prev = dummy, first = head, second, next;
+        dummy.next = head;
+        while(first!=null && first.next!=null)  {
+            second = first.next;
+            next = second.next;
+            prev.next = second;
+            second.next = first;
+            first.next = next;
+            prev = first;
+            first = next;
+        }
+        return dummy.next;
+    }
 }

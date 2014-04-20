b33a0274e3228754790bc3ab08a22415bca56e0c
yalin <ywang28@ncsu.edu>
2014-04-20 07:56:46 -0700

    remove duplicates 2nd

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -10,4 +10,15 @@ public class RemoveDuplicatesFromSortedArray {
         }
         return i+1;
     }
+    public int removeDuplicates2(int[] A) {
+        if(A==null || A.length<2)  return A.length;
+        int start = 0, end = 1;
+        for(; end<A.length; end++)  {
+            if(A[end]!=A[start])  {
+                start++;
+                A[start] = A[end];
+            }
+        }
+        return start+1;
+    }
 }

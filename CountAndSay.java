78ddd043f4aff2b47d749b6c1583b2836c4cf1f9
yalin <ywang28@ncsu.edu>
2014-04-23 08:44:52 -0700

    count and say 2nd

--- CountAndSay.java
+++ CountAndSay.java
@@ -27,4 +27,33 @@ public class CountAndSay {
         }
         return ans.toString();
     }
+    public String countAndSay2(int n) {
+        StringBuilder sb = new StringBuilder("1"), next = new StringBuilder();
+        char last = 'x';
+        int cnt = 1;
+        for(int k=1; k<n; k++)  {
+            for(int i=0; i<sb.length(); i++)  {
+                if(i==0)  {
+                    last = sb.charAt(i);
+                }
+                else if(last == sb.charAt(i))  {
+                    cnt++;
+                }
+                else  {
+                    next.append(cnt + "" + last);
+                    last = sb.charAt(i);
+                    cnt = 1;
+                }
+            }
+            // add last group of chars
+            next.append(cnt + "" + last);
+            // swap two strings
+            StringBuilder tmp = sb;
+            sb = next;
+            next = tmp;
+            next.setLength(0);
+            cnt = 1;
+        }
+        return sb.toString();
+    }
 }

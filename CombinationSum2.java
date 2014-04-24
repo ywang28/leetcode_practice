f27b62ddf786282733d7561ed0cf64bd0b6159c3
yalin <ywang28@ncsu.edu>
2014-04-24 08:39:00 -0700

    combination sum2 2nd

--- CombinationSum2.java
+++ CombinationSum2.java
@@ -26,4 +26,26 @@ public class CombinationSum2 {
         int[] t = {1};
         c.combinationSum2(t,1);
     }
+    public ArrayList<ArrayList<Integer>> combinationSum2_2nd(int[] num, int target) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        if(num==null || num.length==0)  return ret;
+        Arrays.sort(num);
+        combHelper2(num, target, 0, ret, new ArrayList<Integer>());
+        return ret;
+    }
+    public void combHelper2(int[] num, int target, int index, ArrayList<ArrayList<Integer>> ret, ArrayList<Integer> path)  {
+        if(target<0)  return;
+        if(target==0)  {
+            ret.add(new ArrayList<Integer>(path));
+            return;
+        }
+        for(int i=index; i<num.length; i++)  {
+            if(num[i]>target)  return;
+            // remove duplicates
+            if(i>index && num[i]==num[i-1])  continue;
+            path.add(num[i]);
+            combHelper2(num, target-num[i], i+1, ret, path);
+            path.remove(path.size()-1);
+        }
+    }
 }

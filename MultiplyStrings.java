0e7c5cce60ac6cb725bc2970cf968cedac64533c
yalin <ywang28@ncsu.edu>
2014-04-25 08:44:43 -0700

    multiply strings 2nd

--- MultiplyStrings.java
+++ MultiplyStrings.java
@@ -19,4 +19,31 @@ public class MultiplyStrings {
         }
         return s.toString();
     }
+    public String multiply2(String num1, String num2) {
+        if(num1==null || num2==null)  return "";
+        if(num1.length()==0)  return num2;
+        if(num2.length()==0)  return num1;
+        if(num1.equals("0") || num2.equals("0"))  return "0";
+        int len1 = num1.length(), len2 = num2.length();
+        int[] res = new int[len1+len2];
+        for(int i=len1-1; i>=0; i--)  {
+            int n1 = num1.charAt(i) - '0';
+            for(int j=len2-1; j>=0; j--)  {
+                int n2 = num2.charAt(j) - '0';
+                int mul = n1 * n2 + res[i+j+1];
+                res[i+j+1] = mul % 10;
+                res[i+j] += mul / 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        // remove leading 0s
+        int start = 0;
+        while(start < len1+len2 && res[start]==0)  {
+            start++;
+        }
+        for(int i=start; i<len1+len2; i++)  {
+            sb.append(res[i]);
+        }
+        return sb.toString();
+    }
 }

d47d987a5501ac13b73edc964602bfb4c7c8823a
yalin <ywang28@ncsu.edu>
2014-04-28 07:53:42 -0700

    pow 2nd

--- Pow.java
+++ Pow.java
@@ -6,4 +6,10 @@ public class Pow {
         double y = pow(x, n/2);
         return (n % 2 == 0) ? y * y : y * y * ((n > 0) ? x : 1/x);
     }
+    public double pow2(double x, int n) {
+        if(n==0)  return 1.0;
+        double sub1 = pow(x, n/2);
+        double sub2 = (n%2==0) ? sub1 : sub1 * ((n>0) ? x : 1/x);
+        return sub1 * sub2;
+    }
 }

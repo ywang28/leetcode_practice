592d79d37b14c86d00339d3c7bcb995300503f5e
yalin <ywang28@ncsu.edu>
2014-05-01 07:57:30 -0700

    spiral matrix 2nd

--- SpiralMatrix2.java
+++ SpiralMatrix2.java
@@ -27,4 +27,38 @@ public class SpiralMatrix2 {
         }
         return ret;
     }
+    public int[][] generateMatrix2(int n) {
+        if(n<1)  return new int[0][0];
+        int[][] ret = new int[n][n];
+        int sx = 0, sy = 0, ex = n-1, ey = n-1, cnt = 1;
+        while(sx<=ex && sy<=ey)  {
+            // last num left
+            if(sx==ex)  {
+                ret[sx][sy] = cnt;
+            }
+            else  {
+                // go east
+                for(int j=sy; j<ey; j++)  {
+                    ret[sx][j] = cnt++;
+                }
+                // go south
+                for(int i=sx; i<ex; i++)  {
+                    ret[i][ey] = cnt++;
+                }
+                // go west
+                for(int j=ey; j>sy; j--)  {
+                    ret[ex][j] = cnt++;
+                }
+                // go north
+                for(int i=ex; i>sx; i--)  {
+                    ret[i][sy] = cnt++;
+                }
+            }
+            sx++;
+            sy++;
+            ex--;
+            ey--;
+        }
+        return ret;
+    }
 }

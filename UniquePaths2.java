ecc26fccc927e913f00f22687b9db4bb6ce47943
yalin <ywang28@ncsu.edu>
2014-05-02 08:48:27 -0700

    unique path 2 2nd

--- UniquePaths2.java
+++ UniquePaths2.java
@@ -32,4 +32,25 @@ public class UniquePaths2 {
         }
         return dp[m][n];
     }
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        if(obstacleGrid==null || obstacleGrid.length==0 || obstacleGrid[0].length==0 || obstacleGrid[0][0]==1)  return 0;
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        fillDP(obstacleGrid, dp);
+        return dp[m-1][n-1];
+    }
+    public void fillDP(int[][] obstacleGrid, int[][] dp)  {
+        for(int i=1; i<dp.length; i++)  {
+            dp[i][0] = obstacleGrid[i][0]==1  ? 0 : dp[i-1][0];
+        }
+        for(int j=1; j<dp[0].length; j++)  {
+            dp[0][j] = obstacleGrid[0][j]==1 ? 0 : dp[0][j-1];
+        }
+        for(int i=1; i<dp.length; i++)  {
+            for(int j=1; j<dp[0].length; j++)  {
+                dp[i][j] = obstacleGrid[i][j]==1 ? 0 : dp[i][j-1]+dp[i-1][j];
+            }
+        }
+    }
 }

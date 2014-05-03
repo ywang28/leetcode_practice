3fb1cb79771bd6db76c1a8c7f3b5b7d4a4eb4c3e
yalin <ywang28@ncsu.edu>
2014-05-03 08:27:15 -0700

    merge 2 sorted list 2nd

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -19,4 +19,22 @@ public class MergeTwoSortedLists {
         curr.next = (l1 == null) ? l2 : l1;
         return dummy.next;
     }
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if(l1==null)  return l2;
+        if(l2==null)  return l1;
+        ListNode dummy = new ListNode(0), curr = dummy;
+        while(l1!=null && l2!=null)  {
+            if(l1.val < l2.val)  {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = (l1==null) ? l2 : l1;
+        return dummy.next;
+    }
 }

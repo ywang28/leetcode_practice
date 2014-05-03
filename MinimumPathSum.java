f91bed9d2a53be3d1c465f058f8db4966f052013
yalin <ywang28@ncsu.edu>
2014-05-03 08:13:28 -0700

    minimum path sum 2nd

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -19,4 +19,24 @@ public class MinimumPathSum {
         dp[x][y] = grid[x][y] + Math.min(findPathSum(grid,dp,x+1,y), findPathSum(grid,dp,x,y+1));
         return dp[x][y];
     }
+    public int minPathSum2(int[][] grid) {
+        if(grid==null || grid.length==0 || grid[0].length==0)  return 0;
+        int xlen = grid.length, ylen = grid[0].length;
+        int[] dp = new int[ylen];
+        // fill in dp table
+        for(int i=0; i<xlen; i++)  {
+            for(int j=0; j<ylen; j++)  {
+                if(i==0)  {
+                    dp[j] = grid[i][j] + ((j==0) ? 0 : dp[j-1]);
+                }
+                else if(j==0)  {
+                    dp[j] = grid[i][j] + ((i==0) ? 0 : dp[j]);
+                }
+                else  {
+                    dp[j] = grid[i][j] + Math.min(dp[j-1],dp[j]);
+                }
+            }
+        }
+        return dp[ylen-1];
+    }
 }

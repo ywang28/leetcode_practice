44ac0ea686910a9b5fb87ce377271bed9807b28d
yalin <ywang28@ncsu.edu>
2014-05-04 08:29:38 -0700

    plus one 2nd

--- PlusOne.java
+++ PlusOne.java
@@ -16,4 +16,28 @@ public class PlusOne {
         }
         return digits;
     }
+    public int[] plusOne2(int[] digits) {
+        if(digits==null || digits.length==0)  return new int[0];
+        int len = digits.length, i;
+        int[] calc = Arrays.copyOf(digits, len);
+        // find first non 9 digit
+        for(i=len-1; i>=0; i--)  {
+            if(calc[i]==9)  {
+                calc[i] = 0;
+            }
+            else  {
+                calc[i]++;
+                break;
+            }
+        }
+        // need one more digit
+        if(i<0)  {
+            int[] ret = new int[len+1];
+            ret[0] = 1;
+            return ret;
+        }
+        else  {
+            return calc;
+        }
+    }
 }

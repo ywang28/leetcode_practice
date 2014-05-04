1083673a2c4d4a47d96953c189999ee18d4a914f
yalin <ywang28@ncsu.edu>
2014-05-04 08:57:54 -0700

    climb stairs 3rd

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -26,4 +26,16 @@ public class ClimbingStairs {
         dp[n] = climbHelper(n-1, dp) + climbHelper(n-2, dp);
         return dp[n];
     }
+    public int climbStairs3(int n) {
+        if(n<=0)  return 0;
+        if(n==1)  return 1;
+        if(n==2)  return 2;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2; i<n; i++)  {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
 }

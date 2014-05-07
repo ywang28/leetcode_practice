78bb90b790e1dc4b5e899082db1daf0ca12a3197
yalin <ywang28@ncsu.edu>
2014-05-07 08:53:48 -0700

    search 2d matrix 2nd

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -37,4 +37,39 @@ public class Search2DMatrix {
         }
         return false;
     }
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if(matrix==null || matrix.length==0 || matrix[0].length==0)  return false;
+        int xlen = matrix.length, ylen = matrix[0].length;
+        int start = 0, end = xlen-1, mid = start+(end-start)/2;
+        // end will be the row number lies in
+        while(start<=end)  {
+            mid = start + (end-start)/2;
+            if(matrix[mid][0]==target)  {
+                return true;
+            }
+            else if(matrix[mid][0]>target)  {
+                end = mid-1;
+            }
+            else  {
+                start = mid+1;
+            }
+        }
+        if(end<0)  return false;
+        int rowIndex = end;
+        start = 0;
+        end = ylen-1;
+        while(start<=end)  {
+            mid = start + (end-start)/2;
+            if(matrix[rowIndex][mid]==target)  {
+                return true;
+            }
+            else if(matrix[rowIndex][mid]>target)  {
+                end = mid-1;
+            }
+            else  {
+                start = mid+1;
+            }
+        }
+        return false;
+    }
 }

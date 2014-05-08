68234ca0abff2369ed0158a8f923e4b2d91fc40d
yalin <ywang28@ncsu.edu>
2014-05-08 08:16:27 -0700

    combinations 2nd

--- Combinations.java
+++ Combinations.java
@@ -18,4 +18,21 @@ public class Combinations {
             path.remove(path.size()-1);
         }
     }
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        if(n<1 || k>n)  return ret;
+        combHelper(n, k, 1, ret, new ArrayList<Integer>());
+        return ret;
+    }
+    public void combHelper(int n, int k, int index, ArrayList<ArrayList<Integer>> ret, ArrayList<Integer> path)  {
+        if(k==0)  {
+            ret.add(new ArrayList<Integer>(path));
+            return;
+        }
+        for(int i=index; i<=n; i++)  {
+            path.add(i);
+            combHelper(n, k-1, i+1, ret, path);
+            path.remove(path.size()-1);
+        }
+    }
 }

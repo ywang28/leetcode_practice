1b9123120e990e6668ed9b07c8f091a8e8f4a438
yalin <ywang28@ncsu.edu>
2014-05-09 08:10:07 -0700

    word search 2nd

--- WordSearch.java
+++ WordSearch.java
@@ -22,4 +22,29 @@ public class WordSearch {
         board[m][n] = word.charAt(index);
         return false;
     }
+    public boolean exist2(char[][] board, String word) {
+        if(board==null || board.length==0 || board[0].length==0 || word==null || word.length()==0)  return false;
+        for(int i=0; i<board.length; i++)  {
+            for(int j=0; j<board[0].length; j++)  {
+                if(dfs(board, i, j, word, 0))  {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean dfs(char[][] board, int x, int y, String word, int index)  {
+        if(index==word.length())  return true;
+        if(x<0 || y<0 || x>board.length-1 || y>board[0].length-1)  return false;
+        if(board[x][y]!=word.charAt(index))  return false;
+        // set used flag to be '#' and do dfs
+        board[x][y]='#';
+        if(dfs(board, x+1, y, word, index+1) || dfs(board, x, y+1, word, index+1)
+              || dfs(board, x-1, y, word, index+1) || dfs(board, x, y-1, word, index+1))  {
+            return true;
+        }
+        // restore the board
+        board[x][y]=word.charAt(index);
+        return false;
+    }
 }

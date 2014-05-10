39b5e2dc80fc238c6c44cc0136f7640247b7ef7c
yalin <ywang28@ncsu.edu>
2014-05-10 08:08:20 -0700

    search in rotated sorted arr 2nd

--- SearchInRotatedSortedArray2.java
+++ SearchInRotatedSortedArray2.java
@@ -1,4 +1,4 @@
-// wrong answer
+// pass 2nd
 public class SearchInRotatedSortedArray2 {
     public boolean search(int[] A, int target) {
         if(A==null || A.length==0)  return false;
@@ -32,5 +32,43 @@ public class SearchInRotatedSortedArray2 {
     public static void main(String[] Args)  {
         SearchInRotatedSortedArray2 s = new SearchInRotatedSortedArray2();
         System.out.println(s.search(new int[]{3,1},1));
+    }
+    public boolean search2(int[] A, int target) {
+        if(A==null || A.length==0)  return false;
+        int start = 0, end = A.length-1, mid = start + (end-start)/2;
+        while(start<=end)  {
+            mid = start + (end-start)/2;
+            if(A[mid]==target)  return true;
+            // right side is rotated
+            if(A[mid]>A[start])  {
+                // target is within left half
+                if(target>=A[start] && target<A[mid])  {
+                    end = mid-1;
+                }
+                else  {
+                    start = mid+1;
+                }
+            }
+            // left side is rotated
+            else if(A[mid]<A[start])  {
+                if(target>A[mid] && target<=A[end])  {
+                    start = mid+1;
+                }
+                else  {
+                    end = mid-1;
+                }
+            }
+            // remove duplicates
+            else  {
+                while(start<=mid && A[mid]==A[start]) {
+                    start++;
+                }
+                // leftside is not all duplicates
+                if(start<mid)  {
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
     }
 }

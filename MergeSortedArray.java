4c8172a5a77d47b221689b6a591cb8bed137f0bb
yalin <ywang28@ncsu.edu>
2014-05-12 08:37:27 -0700

    merge sorted array 2nd

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -15,4 +15,21 @@ public class MergeSortedArray {
             }
         }
     }
+    public void merge2(int A[], int m, int B[], int n) {
+        if(A==null || B==null || B.length==0 || n<1)  return;
+        for(int i = m-1, j = n-1, k = m+n-1; k>=0; k--)  {
+            if(i<0)  {
+                A[k] = B[j--];
+            }
+            else if(j<0)  {
+                A[k] = A[i--];
+            }
+            else if(A[i]>B[j])  {
+                A[k] = A[i--];
+            }
+            else  {
+                A[k] = B[j--];
+            }
+        }
+    }
 }

b814b81ca33e79f9fd71a04b76d39215c5b11b62
yalin <ywang28@ncsu.edu>
2014-05-17 09:06:12 -0700

    interleaving string 2nd

--- InterleavingString.java
+++ InterleavingString.java
@@ -21,4 +21,24 @@ public class InterleavingString {
         }
         return dp[m][n];
     }
+    public boolean isInterleave2nd(String s1, String s2, String s3) {
+        if(s1==null || s2==null || s3==null)  return false;
+        if(s1.length()+s2.length()!=s3.length())  return false;
+        int len1 = s1.length(), len2 = s2.length();
+        // dp[i][j]:  s3 0 to i+j is interleave of s1 from 0 to i and s2 from 0 to j
+        boolean[][] dp = new boolean[len1+1][len2+1];
+        dp[0][0] = true;
+        for(int i=1; i<=len1; i++)  {
+            dp[i][0] = s1.substring(0,i).equals(s3.substring(0,i));
+        }
+        for(int j=0; j<=len2; j++)  {
+            dp[0][j] = s2.substring(0,j).equals(s3.substring(0,j));
+        }
+        for(int i=0; i<len1; i++)  {
+            for(int j=0; j<len2; j++)  {
+                dp[i+1][j+1] = (dp[i][j+1] && s1.charAt(i) == s3.charAt(i+j+1)) || (dp[i+1][j] && s2.charAt(j) == s3.charAt(i+j+1));
+            }
+        }
+        return dp[len1][len2];
+    }
 }

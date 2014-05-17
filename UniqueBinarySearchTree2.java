b01363969781d134f36c3433ce9f1a65bee62912
yalin <ywang28@ncsu.edu>
2014-05-17 08:21:02 -0700

    unique bst 2 2nd

--- UniqueBinarySearchTree2.java
+++ UniqueBinarySearchTree2.java
@@ -25,4 +25,29 @@ public class UniqueBinarySearchTree2 {
         }
         return ret;
     }
+    public ArrayList<TreeNode> generateTrees2(int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        return generateTrees2nd(1, n);
+    }
+    public ArrayList<TreeNode> generateTrees2nd(int start, int end)  {
+        ArrayList<TreeNode> ret = new ArrayList<TreeNode>();
+        if(start>end)  {
+            ret.add(null);
+            return ret;
+        }
+        for(int i=start; i<=end; i++)  {
+            ArrayList<TreeNode> leftNodes = generateTrees2nd(start,i-1);
+            ArrayList<TreeNode> rightNodes = generateTrees2nd(i+1,end);
+            for(TreeNode left : leftNodes)  {
+                for(TreeNode right : rightNodes)  {
+                    TreeNode curr = new TreeNode(i);
+                    curr.left = left;
+                    curr.right = right;
+                    ret.add(curr);
+                }
+            }
+        }
+        return ret;
+    }
 }

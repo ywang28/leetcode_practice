b582bdc1d67613b81323408962769faa2d1b3197
yalin <ywang28@ncsu.edu>
2014-05-19 07:20:08 -0700

    same tree 2nd

--- SameTree.java
+++ SameTree.java
@@ -4,4 +4,9 @@ public class isSameTree {
         if(p==null || q==null)  return false;
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+    public boolean isSameTree2nd(TreeNode p, TreeNode q) {
+        if(p==null && q==null)  return true;
+        if(p==null || q==null)  return false;
+        return p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
 }

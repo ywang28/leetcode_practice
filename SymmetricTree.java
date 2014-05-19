e6b1b0192e4e8a62a5120eee63ae8a371a95e6fe
yalin <ywang28@ncsu.edu>
2014-05-19 07:37:04 -0700

    symmetric tree 2nd

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -10,4 +10,12 @@ public class SymmetricTree {
         if(leftnode.val!=rightnode.val)  return false;
         return isSymmetricHelper(leftnode.right,rightnode.left);
     }
+    public boolean isSymmetric2nd(TreeNode root) {
+        if(root==null)  return true;
+        return isMirror(root.left, root.right);
+    }
+    public boolean isMirror(TreeNode left, TreeNode right)  {
+        if(left==null || right==null)  return left == right;
+        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
 }

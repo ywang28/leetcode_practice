28b40627d0506c48150fac2af9b8b714b7d61dbb
yalin <ywang28@ncsu.edu>
2014-05-21 07:50:51 -0700

    construct btree from pre-in 2nd

--- ConstructBTreeFromPreIn.java
+++ ConstructBTreeFromPreIn.java
@@ -20,4 +20,22 @@ public class Solution {
         root.right = buildTreeHelper(preorder, s1+leftLen+1, inorder, rootIndex+1, rightLen);
         return root;
     }
+    public TreeNode buildTree2nd(int[] preorder, int[] inorder) {
+        if(preorder==null || preorder.length==0 || inorder==null || inorder.length==0 || preorder.length!=inorder.length)  return null;
+        return buildHelper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+    }
+    private TreeNode buildHelper(int[] preorder, int s1, int e1, int[] inorder, int s2, int e2)  {
+        if(s1 < 0 || e1 > preorder.length-1 || s1 > e1)  return null;
+        TreeNode root = new TreeNode(preorder[s1]);
+        int rootInorder = s2;
+        // find root location in inorder
+        for(; rootInorder <= e2; rootInorder++)  {
+            if(inorder[rootInorder] == preorder[s1])  {
+                break;
+            }
+        }
+        root.left = buildHelper(preorder, s1 + 1, s1 + rootInorder - s2, inorder, s2, rootInorder - 1);
+        root.right = buildHelper(preorder, s1 + rootInorder - s2 + 1, e1, inorder, rootInorder + 1, e2);
+        return root;
+    }
 }

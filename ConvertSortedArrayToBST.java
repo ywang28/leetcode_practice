869825edc8c5711ccdcc570b2df4be55f60625fb
yalin <ywang28@ncsu.edu>
2014-05-22 07:58:59 -0700

    convert  sorted array to BST 2nd

--- ConvertSortedArrayToBST.java
+++ ConvertSortedArrayToBST.java
@@ -11,4 +11,16 @@ public class ConvertSortedArrayToBST {
         root.right = convert(num, mid+1, end);
         return root;
     }
+    public TreeNode sortedArrayToBST2nd(int[] num) {
+        if(num == null || num.length == 0)  return null;
+        return sortedArrayToBST(num, 0, num.length-1);
+    }
+    public TreeNode sortedArrayToBST(int[] num, int start, int end)  {
+        if(start > end)  return null;
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(num[mid]);
+        root.left = sortedArrayToBST(num, start, mid - 1);
+        root.right = sortedArrayToBST(num, mid + 1, end);
+        return root;
+    }
 }

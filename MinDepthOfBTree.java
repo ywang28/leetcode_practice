14273e74c0adab5e75fb31507fa6c383fd7cff4a
yalin <ywang28@ncsu.edu>
2014-05-24 15:54:22 -0700

    min depth of BTree 2nd

--- MinDepthOfBTree.java
+++ MinDepthOfBTree.java
@@ -6,4 +6,9 @@ public class MinDepthOfBTree {
         if(root.right==null)  return minDepth(root.left)+1;
         return Math.min(minDepth(root.left),minDepth(root.right))+1;
     }
+    public int minDepth2nd(TreeNode root) {
+        if(root == null)  return 0;
+        int left = minDepth(root.left), right = minDepth(root.right);
+        return (left == 0) ? right + 1 : ((right == 0) ? left + 1 : Math.min(left, right) + 1);
+    }
 }

a6f7654c74b3d5a2eea3c852d591c302728ba28e
yalin <ywang28@ncsu.edu>
2014-05-24 16:27:53 -0700

    path sum 2nd

--- PathSum.java
+++ PathSum.java
@@ -4,4 +4,10 @@ public class PathSum {
         if(root.left==null && root.right==null)  return root.val==sum;
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
     }
+    public boolean hasPathSum2nd(TreeNode root, int sum) {
+        if(root == null)  return false;
+        if(root.left == null && root.right == null)  return root.val == sum;
+        if(hasPathSum(root.left, sum - root.val))  return true;
+        return hasPathSum(root.right, sum - root.val);
+    }
 }

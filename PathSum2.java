5c746ffee5b91105c122f7e102ff6530afb249f8
yalin <ywang28@ncsu.edu>
2014-05-24 16:26:46 -0700

    path sum2 2nd

--- PathSum2.java
+++ PathSum2.java
@@ -21,4 +21,24 @@ public class PathSum2 {
         pathSumHelper(root.right, sum-root.val, ret, path);
         path.remove(path.size()-1);
     }
+    public ArrayList<ArrayList<Integer>> pathSum2nd(TreeNode root, int sum) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        pathHelper(root, sum, ret, new ArrayList<Integer>());
+        return ret;
+    }
+    public void pathHelper(TreeNode root, int sum, ArrayList<ArrayList<Integer>> ret, ArrayList<Integer> path)  {
+        if(root == null)  return;
+        if(root.left == null && root.right == null)  {
+            if(sum == root.val)  {
+                path.add(root.val);
+                ret.add(new ArrayList<Integer>(path));
+                path.remove(path.size()-1);
+            }
+            return;
+        }
+        path.add(root.val);
+        pathHelper(root.left, sum - root.val, ret, path);
+        pathHelper(root.right, sum - root.val, ret, path);
+        path.remove(path.size()-1);
+    }
 }

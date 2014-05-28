475fbeec6af9c6df10f55be679de3046c2684f70
yalin <ywang28@ncsu.edu>
2014-05-28 08:41:17 -0700

    buy sell stock 2nd

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -18,4 +18,17 @@ public class BestTimeToBuyAndSellStock {
         profit = Math.max(profit, high-low);
         return profit;
     }
+    public int maxProfit2nd(int[] prices) {
+        if(prices == null || prices.length < 2)  return 0;
+        int min = prices[0], profit = 0;
+        for(int i = 1; i < prices.length; i++)  {
+            if(prices[i] <= min)  {
+                min = prices[i];
+            }
+            else  {
+                profit = Math.max(profit, prices[i] - min);
+            }
+        }
+        return profit;
+    }
 }

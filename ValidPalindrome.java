5da301223868b745f50ef9ceaab0807c5ac43c39
yalin <ywang28@ncsu.edu>
2014-05-30 08:35:38 -0700

    valid palindrome 3rd

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -11,7 +11,7 @@ public class ValidPalindrome {
         }
         return true;
     }
-    public boolean isPalindrome(String s) {
+    public boolean isPalindrome2nd(String s) {
         if(s==null || s.length()==0)  return true;
         int i=0, j=s.length()-1;
         String s2 = s.toLowerCase();
@@ -29,6 +29,27 @@ public class ValidPalindrome {
                 i++;
                 j--;
             }
+        }
+        return true;
+    }
+    public boolean isPalindrome3rd(String s) {
+        if(s == null)  return false;
+        if(s.length() < 2)  return true;
+        String lower = s.toLowerCase();
+        int i = 0, j = s.length() - 1;
+        while(i < j)  {
+            char front = lower.charAt(i), end = lower.charAt(j);
+            if(!Character.isLetterOrDigit(front))  {
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(end))  {
+                j--;
+                continue;
+            }
+            if(front != end)  return false;
+            i++;
+            j--;
         }
         return true;
     }

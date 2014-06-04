a56d42e8ec2c09555846ad19611e53861d65aad7
yalin <ywang28@ncsu.edu>
2014-06-04 07:53:04 -0700

    single num 2nd

--- SingleNumber.java
+++ SingleNumber.java
@@ -7,4 +7,17 @@ public class SingleNumber {
         }
         return ret;
     }
+    public int singleNumber2nd(int[] A) {
+        // logic expression:  A ^ A = 0
+        // 0 ^ B = B
+        // Array:    AABCCDDDD
+        // xor of all numbers
+        if(A == null || A.length==0)  return -1;
+        if(A.length == 1)  return A[0];
+        int ret = 0;
+        for(int i : A)  {
+            ret ^= i;
+        }
+        return ret;            
+    }
 }

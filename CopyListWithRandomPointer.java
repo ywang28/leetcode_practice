abc2b503c0c095f4fcdcd6ff821cbb2588a7aabd
yalin <ywang28@ncsu.edu>
2014-06-04 08:07:27 -0700

    copy list with random pointers 2nd

--- CopyListWithRandomPointer.java
+++ CopyListWithRandomPointer.java
@@ -12,4 +12,18 @@ public class CopyListWithRandomPointer {
         ret.random = copyListHelper(head.random,nodemap);
         return ret;
     }
+    public RandomListNode copyRandomList2nd(RandomListNode head) {
+        if( head == null )   return null;
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        return copyListHelper(head, map);        
+    }
+    private RandomListNode copyListHelper2nd(RandomListNode head, HashMap<RandomListNode, RandomListNode> map)  {
+        if( head == null )  return null;
+        if(map.containsKey(head))  return map.get(head);
+        RandomListNode newNode = new RandomListNode(head.label);
+        map.put(head, newNode);
+        newNode.next = copyListHelper2nd(head.next, map);
+        newNode.random = copyListHelper2nd(head.random, map);
+        return newNode;
+    }
 }

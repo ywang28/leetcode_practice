939a8b2609ba64d836171f22abaab240c60af58a
yalin <ywang28@ncsu.edu>
2014-06-09 09:01:24 -0700

    postorder traversal 2nd

--- BinaryTreePostorderTraversal.java
+++ BinaryTreePostorderTraversal.java
@@ -21,5 +21,21 @@ public class BinaryTreePostorderTraversal {
         }
         return ret;
     }
+    public List<Integer> postorderTraversal2nd(TreeNode root) {
+        ArrayList<Integer>  ret = new ArrayList<Integer>();
+           if (root == null)  return ret;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        Stack<TreeNode> st2 = new Stack<TreeNode>();
+st.push(root);
+        while (!st.isEmpty())  {
+            TreeNode curr = st.pop();
+            st2.push(curr);
+            if (curr.left != null)   st.push(curr.left);
+            if (curr.right != null)   st.push(curr.right);
+        }
+        while(!st2.isEmpty())  {
+            ret.add(st2.pop().val);
+        }
+        return ret;
+    }
 }
-    

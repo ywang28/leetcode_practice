71b3f58e9499ce88f96c2836d3a2588c4d219d36
yalin <ywang28@ncsu.edu>
2014-06-12 08:29:25 -0700

    sort list 2nd

--- SortList.java
+++ SortList.java
@@ -31,4 +31,41 @@ public class SortList {
         prev.next = null;
         return slow;
     }
+    public ListNode sortList2nd(ListNode head) {
+           if (head == null || head.next == null)  return head;
+        // more than one node
+        // find the mid point of the list. parent is parent to the slow pointer
+        ListNode dummy = new ListNode(0), slow = dummy, fast = head;
+        dummy.next = head;
+        // edge case:  1 -> 2 -> null
+        // after loop, slow is parent to mid point
+        while (fast != null && fast.next != null)  {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        ListNode leftHead = sortList(head), rightHead = sortList(mid);
+        return mergeList(leftHead, rightHead);        
+    }
+    private ListNode mergeList(ListNode left, ListNode right)  {
+        // deal with edge case
+        if (left == null)   return right;
+        if (right == null)   return left;
+        // go through list and merge
+        ListNode dummy = new ListNode(0), mergeHead = dummy;
+        while (left != null && right != null)  {
+            if (left.val < right.val)  {
+                dummy.next = left;
+                left = left.next;
+            }
+            else  {
+                dummy.next = right;
+                right = right.next;
+            }
+            dummy = dummy.next;
+        }
+        dummy.next = (left == null) ? right : left;
+        return mergeHead.next;
+    }
 }
